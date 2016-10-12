90bd3d95390797db9cd962c7a6c72ec72ee9dd4c
Belozerova <dabel@merann.ru>
2016-10-12 15:03:29 +0300

    Sort

--- exercise3/com/dashik/deanery/MainProgram.java
+++ exercise3/com/dashik/deanery/MainProgram.java
@@ -1,5 +1,6 @@
 package com.dashik.deanery;
 import java.util.ArrayList;
+import java.util.*;
 
 class MainProgram {
 	public static void main(String[] arg) {
@@ -8,10 +9,10 @@ class MainProgram {
 		ArrayList<EducationalRoom> forLecture = new ArrayList<EducationalRoom>();
 		
 		try {
-			depRooms[0] = new DeansOffice("Vasin",1,null);
-			depRooms[1] = new LectureRoom(30,12,"131");
-			depRooms[2] = new LectureRoom(30,2,"2");
-			depRooms[3] = new LectureRoom(30,3,"3");
+			depRooms[0] = new DeansOffice("Vasin",1,"14");
+			depRooms[1] = new LectureRoom(30,12,"121");
+			depRooms[2] = new LectureRoom(30,2,"22");
+			depRooms[3] = new LectureRoom(30,2,"21");
 			
 			for (int i=0; i<4; i++) {
 			depRooms[i].open();
@@ -20,25 +21,57 @@ class MainProgram {
 		
 			for (int i=0; i<4; i++) {
 							
-				if(depRooms[i].getClass().getName().equals("com.dashik.deanery.LectureRoom")) {
+				/*if(depRooms[i].getClass().getName().equals("com.dashik.deanery.LectureRoom")) {*/
 				forLecture.add(depRooms[i]);
-				}				
+				/*}		*/		
 			}
 			
 			
 			
-			for (int i=0; i<depRooms.length; i++) {
+			Collections.sort(forLecture, new Comparator<EducationalRoom>() {
+				@Override
+				public int compare(EducationalRoom room1, EducationalRoom room2)
+				{	
+					int result = room2.getClass().getName().compareTo(room1.getClass().getName());
+					if (result != 0) {
+						return (int)(result/Math.abs(result));
+					}
+					result = room2.roomnumber.compareTo(room1.roomnumber);
+					if(result != 0) {
+						return (int)(result/Math.abs(result));
+					}
+					
+					return (result != 0) ? (int)(result/Math.abs(result)) : 0;
+					
+				}
+			});
+			
+			
+			/*Collections.sort(forLecture, new Comparator<EducationalRoom>() {
+				@Override
+				public int compare(EducationalRoom room1, EducationalRoom room2){	
+					return room1.roomnumber.compareTo(room2.roomnumber);
+				}
+			});*/
+			
+			for (int i=0; i<forLecture.size(); i++) {
+				System.out.println(forLecture.get(i).roomnumber);
+			}
+			
+			
+			
+			/*for (int i=0; i<depRooms.length; i++) {
 				for (int j=0; j<forLecture.size(); j++) {
 					if((depRooms[i].getClass().getName().equals("com.dashik.deanery.DeansOffice")) 
 					&& (depRooms[i].floor==forLecture.get(j).floor) && (depRooms[i].roomnumber==forLecture.get(j).roomnumber)) {
 						throw new DuplicateRoomNumberException();
 					}
 				} 
-			}		
-		}	
+			}*/		
+		}	/*
 		catch(DuplicateRoomNumberException e) {
 			System.out.println("Duplicated room found");
-		}	
+		}*/	
 		catch (IllegalArgumentException e){
 			System.out.println(e.getMessage());
 			/*return;*/
@@ -47,6 +80,9 @@ class MainProgram {
 			System.out.println(e.getMessage());
 		}
 		System.out.println("Program was continued.");
+		
+		
+		
 	}
 		
 		
